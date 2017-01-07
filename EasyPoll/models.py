a25b8797835d83df67a135a979795c51ed70663a
Arthur Revaclier <a.r.revaclier@student.tue.nl>
2017-01-07 22:21:14 +0100

    Simple poll, not working

--- EasyPoll/models.py
+++ EasyPoll/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
 # Create your models here.
+class Question(models.Model):
+    text = models.CharField(max_length=255)
+    published_date = models.DateTimeField("Published date")
+
+    def __str__(self):
+        return self.text
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text = models.CharField(max_length=255)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.text
--- EasyPoll/views.py
+++ EasyPoll/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from .models import Question, Answer
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
 
 # Create your views here.
+class IndexView(generic.ListView):
+    template_name = 'EasyPoll/index.html'
+    context_object_name = 'latest_question'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-published_date')[:1]
+
+class QuestionView(generic.DetailView):
+    model = Question
+    template_name = 'EasyPoll/question.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'EasyPoll/results.html'
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.answer_set.get(pk=request.POST['choice'])
+    except (KeyError, Answer.DoesNotExist):
+        return render(request, 'EasyPoll/question.html', {'question_detail':question, 'error_message':'Please choose'})
+
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse('EasyPoll:results', args=(question_id,)))
